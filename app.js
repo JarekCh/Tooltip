a53dd558c6ea0d621bb46e0d6767ab64e122d3f3
Jaroslaw Charchula <charchula@gmail.com>
2022-05-13 19:06:37 +0200

    creatae listeners and function for add and romove. early version.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // app.js
+const boxes = document.querySelectorAll('.boxes div');
+
+const createTooltip = (e) => {
+    const tooltipParent = e.target;
+    const tooltipText = tooltipParent.dataset.tooltip;
+
+    const newTooltip = document.createElement('span');
+    newTooltip.innerHTML = tooltipText;
+    newTooltip.className = "tooltip"
+
+    tooltipParent.appendChild(newTooltip);
+}
+
+const removeTooltip = (e) => {
+    const tooltip = e.target.querySelector(".tooltip");
+    if (tooltip) {
+        tooltip.remove();
+    }
+};
+
+boxes.forEach((box) => box.addEventListener('mouseover', createTooltip));
+boxes.forEach((box) => box.addEventListener('mouseleave', removeTooltip));
